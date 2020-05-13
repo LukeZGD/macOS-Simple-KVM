25ad09a81f5b471f08a0fb997b0829411f15ddf2
LukeeGD <l32luke@gmail.com>
2020-05-13 21:30:53 +0800

    ...

--- basic.sh
+++ basic.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
 OSK="ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc"
-VMDIR=$PWD
+VMDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 OVMF=$VMDIR/firmware
 #export QEMU_AUDIO_DRV=pa
 #QEMU_AUDIO_DRV=pa
+BIND_PID1="8086 9d2f"
+BIND_BDF1="0000:00:14.0"
+BIND_PID2="8086 9d31"
+BIND_BDF2="0000:00:14.2"
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root"
+    exit
+fi
+
+modprobe vfio-pci
+echo "$BIND_PID1" > /sys/bus/pci/drivers/vfio-pci/new_id
+echo "$BIND_BDF1" > /sys/bus/pci/devices/$BIND_BDF1/driver/unbind
+echo "$BIND_BDF1" > /sys/bus/pci/drivers/vfio-pci/bind
+echo "$BIND_PID1" > /sys/bus/pci/drivers/vfio-pci/remove_id
+echo "$BIND_PID2" > /sys/bus/pci/drivers/vfio-pci/new_id
+echo "$BIND_BDF2" > /sys/bus/pci/devices/$BIND_BDF2/driver/unbind
+echo "$BIND_BDF2" > /sys/bus/pci/drivers/vfio-pci/bind
+echo "$BIND_PID2" > /sys/bus/pci/drivers/vfio-pci/remove_id
 
 qemu-system-x86_64 \
     -enable-kvm \
@@ -22,7 +40,17 @@ qemu-system-x86_64 \
     -netdev user,id=net0 \
     -device e1000-82545em,netdev=net0,id=net0,mac=52:54:00:c9:18:27 \
     -device ich9-ahci,id=sata \
-    -drive id=ESP,if=none,format=qcow2,file=ESP.qcow2 \
+    -drive id=ESP,if=none,format=qcow2,file=$VMDIR/ESP.qcow2 \
     -device ide-hd,bus=sata.2,drive=ESP \
-    -drive id=InstallMedia,format=raw,if=none,file=BaseSystem.img \
+    -drive id=InstallMedia,format=raw,if=none,file=$VMDIR/BaseSystem.img \
     -device ide-hd,bus=sata.3,drive=InstallMedia \
+    -drive id=SystemDisk,if=none,file=$VMDIR/vol.qcow2 \
+    -device ide-hd,bus=sata.4,drive=SystemDisk \
+    -device pcie-root-port,bus=pcie.0,multifunction=on,port=1,chassis=1,id=port.1 \
+    -device vfio-pci,host=00:14.0,bus=port.1 \
+    -display sdl -full-screen \
+    2>/dev/null
+
+echo "$BIND_BDF1" > /sys/bus/pci/drivers/vfio-pci/unbind
+echo "$BIND_BDF1" > /sys/bus/pci/drivers/xhci_hcd/bind
+echo "$BIND_BDF2" > /sys/bus/pci/drivers/vfio-pci/unbind
