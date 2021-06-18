0cbb02ecce4c2461408da79021922a5182c99e0d
LukeZGD <l32luke@gmail.com>
2021-06-18 18:00:58 +0800

    Update

--- basic.sh
+++ basic.sh
@@ -3,20 +3,20 @@
 OSK="ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc"
 VMDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 OVMF=$VMDIR/firmware
-export QEMU_AUDIO_DRV=pa
-QEMU_AUDIO_DRV=pa
+#export QEMU_AUDIO_DRV=pa
+#QEMU_AUDIO_DRV=pa
 BIND_PID1="8086 9d2f"
 BIND_BDF1="0000:00:14.0"
 BIND_PID2="8086 9d31"
 BIND_BDF2="0000:00:14.2"
 
 echo $VMDIR
-if [ "$EUID" != 0 ]; then
+if [[ "$EUID" != 0 && $1 == "vfio" ]]; then
     echo "Please run as root"
-    exit
+    exit 1
 fi
 
-if [[ $1 == vfio ]]; then
+if [[ $1 == "vfio" ]]; then
     modprobe vfio-pci
     echo "$BIND_PID1" > /sys/bus/pci/drivers/vfio-pci/new_id
     echo "$BIND_BDF1" > /sys/bus/pci/devices/$BIND_BDF1/driver/unbind
@@ -26,7 +26,11 @@ if [[ $1 == vfio ]]; then
     echo "$BIND_BDF2" > /sys/bus/pci/devices/$BIND_BDF2/driver/unbind
     echo "$BIND_BDF2" > /sys/bus/pci/drivers/vfio-pci/bind
     echo "$BIND_PID2" > /sys/bus/pci/drivers/vfio-pci/remove_id
-    vfio="-device pcie-root-port,bus=pcie.0,multifunction=on,port=1,chassis=1,id=port.1 -device vfio-pci,host=00:14.0,bus=port.1"
+    extra="-device pcie-root-port,bus=pcie.0,multifunction=on,port=1,chassis=1,id=port.1 \
+           -device vfio-pci,host=00:14.0,bus=port.1"
+elif [[ $1 == "install" ]]; then
+    extra="-drive id=InstallMedia,format=raw,if=none,file=$VMDIR/BaseSystem.img \
+           -device ide-hd,bus=sata.3,drive=InstallMedia"
 fi
 
 if [[ ! -e $VMDIR/firmware/OVMF_VARS-1024x768.fd ]]; then
@@ -49,15 +53,13 @@ qemu-system-x86_64 \
     -netdev user,id=net0 \
     -device e1000-82545em,netdev=net0,id=net0,mac=52:54:00:c9:18:27 \
     -device ich9-ahci,id=sata \
-    -drive id=ESP,if=none,format=qcow2,file=$VMDIR/ESP.qcow2 \
+    -drive id=ESP,if=none,format=qcow2,file=$VMDIR/OpenCore-nopicker.qcow2 \
     -device ide-hd,bus=sata.2,drive=ESP \
-    -drive id=InstallMedia,format=raw,if=none,file=$VMDIR/BaseSystem.img \
-    -device ide-hd,bus=sata.3,drive=InstallMedia \
     -drive id=SystemDisk,if=none,file=$VMDIR/vol.qcow2 \
     -device ide-hd,bus=sata.4,drive=SystemDisk \
-    $vfio
+    $extra
 
-if [[ $1 == vfio ]]; then
+if [[ $1 == "vfio" ]]; then
     echo "$BIND_BDF1" > /sys/bus/pci/drivers/vfio-pci/unbind
     echo "$BIND_BDF1" > /sys/bus/pci/drivers/xhci_hcd/bind
     echo "$BIND_BDF2" > /sys/bus/pci/drivers/vfio-pci/unbind
